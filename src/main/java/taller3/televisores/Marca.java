e26f2e38cb390568a32c009f4175317bf2e1a09d
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-10 22:30:58 -0500

    Create Marca.java

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,18 @@
+package taller3.televisores;
+
+public class Marca {
+  private String nombre;
+
+  public Marca (String nombre){
+    this.nombre = nombre;
+  }
+
+  public setNombre (nombre){
+    this.nombre = nombre;
+  }
+
+  public getNombre(){
+    return this.nombre;
+  }
+  
+}

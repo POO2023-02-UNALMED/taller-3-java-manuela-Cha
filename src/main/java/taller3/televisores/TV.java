7b05210547f6dcc23a130c9ac234f2854d7e656e
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-10 23:10:48 -0500

    Create TV.java

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,59 @@
+package taller3.televisores;
+
+public class TV {
+  private Marca marca;
+  private int canal = 1;
+  private int precio = 500;
+  boolean estado;
+  private volumen = 1;
+  private Control control;
+  static int numTV;
+
+  public TV (Marca marca, boolean estado){
+    this.marca = marca;
+    this.estado = estado;
+    ++TV.numTV;
+  }
+
+  public void setMarca (Marca marca){
+    this.marca = marca;
+  }
+  public Marca getMarca(){
+    return this.marca;
+  }
+  
+  public void setCanal(int canal){
+    this.canal = canal;
+  }
+  public int getCanal(){
+    return this.canal;
+  }
+  
+  public void setPrecio(int precio){
+    this.precio = precio;
+  }
+  public int getPrecio(){
+    return this.precio;
+  }
+  
+  public void setVolumen (int volumen){
+    this.volumen = volumen;
+  }
+  public int getVolumen (){
+    return this.volumen;
+  }
+
+  public void setControl (Control control){
+    this.control = control;
+  }
+  public control getControl (){
+    return this.control;
+  }
+
+  public static void setNumTV(int numero){
+    TV.numTV = numero;
+  }
+  public static int get (){
+    return TV.numTV;
+  }
+}

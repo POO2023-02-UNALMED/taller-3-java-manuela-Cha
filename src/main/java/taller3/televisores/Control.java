de6c4ac70f415e96b7d17262b31653156bd8b3fd
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-11 19:44:31 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,37 +1,45 @@
 package taller3.televisores;
 
 public class Control (){
-  TV tv;
+  private TV tv;
+    
+  public void enlazar(TV televisor){
+    this.tv = televisor}  
   
+  public void turnOn(){
+    this.estado = true;
+  }
+  public void turnOf(){
+    this.estado = false;
+  }
+    
+  public void canalUp(){
+    ++this.canal;
+  }
+  public void canalDown(){
+    --this.canal;
+  }
+  
+  public void volumenUp(){
+    ++this.volumen;
+  }
+  public void volumenDown(){
+    --this.volumen;
+  }
+    
+  public void setCanal(int canal){
+    if (canal > 0 && canal < 121  && this.estado == true){
+      this.canal = canal;}
+  }
+  public void setVolumen (int volumen){
+    if (volumen > 0 && volumen < 8 && this.estado == true){
+      this.volumen = volumen;}
+  }
 
-public void turnOn(){
-  this.estado = true;
+  public TV setTV(TV televisor){
+    this.tv = televisor;
   }
-public void turnOf(){
-  this.estado = false;
+  public void getTV (){
+    return this.tv;
   }
-  
-public void canalUp(){
-  ++this.canal;
-  }
-public void canalDown(){
-  --this.canal;
-  }
-
-public void volumenUp(){
-  ++this.volumen;
 }
-public void volumenDown(){
-  --this.volumen;
-}
-  
-public void setCanal(int canal)
-  if (canal > 0 && canal < 121  && this.estado == true){
-    this.canal = canal;
-    }
-public void setVolumen (int volumen)
-  if (volumen > 0 && volumen < 8 && this.estado == true){
-    this.volumen = volumen;
-    }
-}
-  
